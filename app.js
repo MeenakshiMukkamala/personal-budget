f8395f6e26b12447784864c9d7aac2844a19967b
Meenakshi Mukkamala <meenakm@uci.edu>
2025-09-04 16:51:47 -0700

    Transfer money between envelopes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 let envelopes = [];
 let totalBudget = 0;
 
+//Create a new envelope
 app.post('/envelopes', (req, res) => {
   const { category, amount } = req.body;
   let newEnvelope = { id: envelopes.length + 1, category, amount }
@@ -23,9 +24,8 @@ app.post('/envelopes', (req, res) => {
 
 });
 
+//Get a specific envelope by ID
 app.get('/envelopes/:id', (req, res) => {
-    console.log(envelopes)
-
   const envelope = envelopes.find(e => e.id === parseInt(req.params.id));
   if (!envelope) {
     return res.status(404).json({ message: 'Envelope not found' });
@@ -33,10 +33,12 @@ app.get('/envelopes/:id', (req, res) => {
   res.json(envelope);
 });
 
+//Get all envelopes and total budget
 app.get('/envelopes', (req, res) => {
   res.json({ envelopes, totalBudget });
 });
 
+//Withdraw money from an envelope
 app.post('/envelopes/withdraw', (req, res) => {
   const { id, amount } = req.body;
   const envelope = envelopes.find(e => e.id === parseInt(id));
@@ -56,12 +58,32 @@ app.post('/envelopes/withdraw', (req, res) => {
 }
 );
 
+//Delete an envelope
 app.post('envelopes/delete', (req, res) => {
   const { id } = req.body;
   envelopes = envelopes.filter(e => e.id != parseInt(id));
 });
 
+//Transfer money between envelopes
+app.post('/envelopes/transfer/:from/:to', (req, res) => {
+  const envelope_from = envelopes.find(e => e.id === parseInt(req.params.from));
+  const envelope_to = envelopes.find(e => e.id === parseInt(req.params.to));
+  if (!envelope_from) {
+    return res.status(404).json({ message: 'Envelope from not found' });
+  } 
+  if (!envelope_to) {
+    return res.status(404).json({ message: 'Envelope to not found' });
+  }
+  envelope_from.amount -= req.body.amount;
+  envelope_to.amount += req.body.amount;
+  res.json({
+    message: 'Transfer successful',
+    envelope_from,
+    envelope_to
+  });
+});
 
+//Start the server
 app.listen(PORT, () => {
   console.log('Server running at http://localhost:3000/');
 });
