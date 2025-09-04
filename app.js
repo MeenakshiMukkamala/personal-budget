6051ce55b23904b4b4a5bdd5afc48987e024b7de
Meenakshi Mukkamala <meenakm@uci.edu>
2025-09-04 16:08:09 -0700

    Get individual envelope by id

--- app.js
+++ app.js
@@ -4,20 +4,37 @@ const PORT = 3000;
 
 app.use(express.json());
 
+
+
 let envelopes = [];
 let totalBudget = 0;
 
 app.post('/envelopes', (req, res) => {
   const { category, amount } = req.body;
-  envelopes.push({ id: envelopes.length + 1, category, amount });
+  let newEnvelope = { id: envelopes.length + 1, category, amount }
+  console.log(envelopes)
+  envelopes.push(newEnvelope);
   totalBudget += amount;
-
   res.status(201).json({
     message: 'Envelope created',
-    envelope: envelopes[0],
+    envelope: newEnvelope,
     totalBudget
   }); 
 
+});
+
+app.get('/envelopes/:id', (req, res) => {
+    console.log(envelopes)
+
+  const envelope = envelopes.find(e => e.id === parseInt(req.params.id));
+  if (!envelope) {
+    return res.status(404).json({ message: 'Envelope not found' });
+  } 
+  res.json(envelope);
+});
+
+app.get('/envelopes', (req, res) => {
+  res.json({ envelopes, totalBudget });
 });
 
 app.listen(PORT, () => {
