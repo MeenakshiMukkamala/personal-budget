14ddf0e08ba85ba4c43f02ec5db95d2f5af740e7
Meenakshi Mukkamala <meenakm@uci.edu>
2025-09-04 15:55:52 -0700

    Post request for individual envelopes

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.json());
 
+let envelopes = [];
+let totalBudget = 0;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}
-)
+app.post('/envelopes', (req, res) => {
+  const { category, amount } = req.body;
+  envelopes.push({ id: envelopes.length + 1, category, amount });
+  totalBudget += amount;
+
+  res.status(201).json({
+    message: 'Envelope created',
+    envelope: envelopes[0],
+    totalBudget
+  }); 
+
+});
 
 app.listen(PORT, () => {
   console.log('Server running at http://localhost:3000/');
 });
+
+
